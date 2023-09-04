6f75d4da4e33aded15c14693842686719ec57d4f
DINHOMACA <ehfernandes79@gmail.com>
2023-09-04 10:39:46 -0300

    OrderStatus enum

---
+++ src/main/java/com/hanielprojects/course/entities/enums/OrderStatus.java
@@ -0,0 +1,37 @@
+package com.hanielprojects.course.entities.enums;
+
+import com.hanielprojects.course.entities.Order;
+
+public enum OrderStatus {
+	
+	WAITTING_PAYMENT(1),
+	PAID(2),
+	SHIPPED(3),
+	DELIVERED(4),
+	CANCELED(4);
+	
+	private int code;
+	
+	private OrderStatus(int code) {
+		this.code = code;
+	}
+
+	
+	public int getCode() {
+		return code;
+	}
+	
+	public static OrderStatus valueOf(int code) {
+		
+		for (OrderStatus value : OrderStatus.values()) {
+			
+			if(value.getCode() == code) {
+				return value;
+			}
+		}
+		throw new IllegalArgumentException("Invalid OrderStatus code");
+		
+	}
+	
+	
+}
